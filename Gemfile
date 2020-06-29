4cbd183b82b8dea8e312b0d38f3f6e5e26c219fe
Edgar Marques <edgar.eu1@hotmail.com>
2020-06-29 18:37:34 +0100

    Create the form with simple_form

--- Gemfile
+++ Gemfile
@@ -50,5 +50,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-# create users
+# Create users
 gem 'devise'
+
+# Stylish forms
+gem 'simple_form'
--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,22 @@
 class LogsController < ApplicationController
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+    @log.user = current_user
+
+    if @log.save
+      redirect_to logs_path
+    else
+      render 'logs/new'
+    end
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :location_id)
+  end
 end
